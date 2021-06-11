3bc3bdd45842c397cc56b3c317a59a890176ec54
Mehrdad Shahidi <m.mehrdadshahidi@gmail.com>
2021-06-11 23:11:44 +0430

    config.go

--- main.go
+++ main.go
@@ -1,32 +1,21 @@
 package main
 
 import (
+	"./bot"
+	"./config"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
 )
 
-const token string = "ODUyMjE5NjIyMTgxOTYxNzY2.YMDpXw.1wapy2cEGr40CKO0P28glKxQZYA"
-
-var BotID string
-
 func main() {
-	dg, err := discordgo.New("Bot " + token)
-	if err != nil {
-		fmt.Print(err.Error())
+	err := config.ReadConfig()
+	
+	if err !=nil {
+		fmt.Println(err.Error())
 		return
 	}
-
-	u, err := dg.User("@me")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	BotID := u.ID
-	err = dg.Open()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println("Bot is runnig", BotID)
+	bot.Start()
 	<- make(chan struct{})
+	return
+
 
 }
